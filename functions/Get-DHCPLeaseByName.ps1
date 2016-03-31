a47dbfc79a0d6bd1a2008d728cdce063977229ee
Artem Iarmoliuk <koolgen@gmail.com>
2016-03-31 20:05:11 +0300

    Add cimsession management: 1. Catching Access exception to stop script 2. Remove cimsession after script ends

--- functions/Get-DHCPLeaseByName.ps1
+++ functions/Get-DHCPLeaseByName.ps1
@@ -26,11 +26,16 @@ function Get-DHCPLeaseByName {
     $server = netsh dhcp show server | Select-String $net | ForEach-Object {$_ -split " "} | Select-Object -Index 1
     $server = $server -replace "\[", '' -replace "\]", ''
     #Open simsession to dhcpserver because DHCP server cmdlets dose not support Credentail parameter
-    $session = New-CimSession -ComputerName $server -Credential $Credential
+    try {
+        $session = New-CimSession -ComputerName $server -Credential $Credential -ErrorAction Stop
+    } catch [Microsoft.Management.Infrastructure.CimException] {
+        throw "Access is denied. Use DHCP Admin credentials"
+    }
     #Find scopes for this DHCP server
     $scopes = Get-DhcpServerv4Scope -CimSession $session
     #For each scope parse DHCP leases and find matching Name
     foreach ($scope in $scopes) {
         Get-DhcpServerv4Lease -CimSession $session -ScopeId $scope.ScopeId | where {$_.HostName -match $Name} | select HostName, IPAddress, AddressState, LeaseExpiryTime
     }
+    Remove-CimSession $session
 }; New-Alias -Name lease -Value Get-DHCPLeaseByname
