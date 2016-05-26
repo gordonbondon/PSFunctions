d291bf0ae3f1c61bec01eb9951b95fa25044dc9f
Artem Iarmoliuk <koolgen@gmail.com>
2016-05-26 11:17:58 +0300

    Make Credential param mandatory. Fix aliases.

--- functions/Get-DHCPLeaseByName.ps1
+++ functions/Get-DHCPLeaseByName.ps1
@@ -13,15 +13,16 @@ Specify Computer name
 function Get-DHCPLeaseByName {
     [CmdletBinding()]
     param (
-         [string]
-         $Name,
-         [System.Management.Automation.CredentialAttribute()]
-         $Credential
+         [Parameter(Mandatory=$true)]
+         [string]$Name,
+         [Parameter(Mandatory=$true)]
+         [PSCredential]
+         [System.Management.Automation.CredentialAttribute()]$Credential
     )
 
     #Find first DHCP server
-    $ipaddress = Get-NetIPAddress -InterfaceAlias 'Ethernet' -AddressFamily 'IPv4' | select IPAddress
-    $net = ($ipaddress.IPAddress.Split(".", 3) | select -Index 0,1) -join "."
+    $ipaddress = Get-NetIPAddress -InterfaceAlias 'Ethernet' -AddressFamily 'IPv4' | Select-Object IPAddress
+    $net = ($ipaddress.IPAddress.Split(".", 3) | Select-Object -Index 0,1) -join "."
 
     $server = netsh dhcp show server | Select-String $net | ForEach-Object {$_ -split " "} | Select-Object -Index 1
     $server = $server -replace "\[", '' -replace "\]", ''
@@ -35,7 +36,9 @@ function Get-DHCPLeaseByName {
     $scopes = Get-DhcpServerv4Scope -CimSession $session
     #For each scope parse DHCP leases and find matching Name
     foreach ($scope in $scopes) {
-        Get-DhcpServerv4Lease -CimSession $session -ScopeId $scope.ScopeId | where {$_.HostName -match $Name} | select HostName, IPAddress, AddressState, LeaseExpiryTime
+        Get-DhcpServerv4Lease -CimSession $session -ScopeId $scope.ScopeId |
+            Where-Object {$_.HostName -match $Name} |
+            Select-Object HostName, IPAddress, AddressState, LeaseExpiryTime
     }
     Remove-CimSession $session
 }; New-Alias -Name lease -Value Get-DHCPLeaseByname
