1d76f530aabdc741466e9542631dc211b50a617c
Artem Iarmoliuk <koolgen@gmail.com>
2017-03-09 11:09:01 +0200

    Allow specifying DHCP server

--- Functions/Get-DHCPLeaseByName.ps1
+++ Functions/Get-DHCPLeaseByName.ps1
@@ -8,6 +8,16 @@ Parses DHCP database to get DHCP lease IP address by Computer name
 .PARAMETER Name
 Specify Computer name
 
+.PARAMETER MAC
+MAc address of a computer to find
+
+.PARAMETER Server
+Set dchp server address.
+If not set function will try to detect your dhcp server
+
+.PARAMETER Credential
+Provide creadentials of DHCP admin
+
 #>
 
 function Get-DHCPLeaseByName {
@@ -21,6 +31,11 @@ function Get-DHCPLeaseByName {
          [string]
          $MAC,
 
+         [Parameter(Mandatory=$false,Position=2,ParameterSetName='Name')]
+         [Parameter(Mandatory=$false,Position=2,ParameterSetName='MAC')]
+         [string]
+         $Server,
+
          [Parameter(Mandatory=$true,Position=1,ParameterSetName='Name')]
          [Parameter(Mandatory=$true,Position=1,ParameterSetName='MAC')]
          [PSCredential]
@@ -32,14 +47,18 @@ function Get-DHCPLeaseByName {
     $ipaddress = Get-NetIPAddress -InterfaceAlias 'Ethernet' -AddressFamily 'IPv4' | Select-Object IPAddress
     $net = ($ipaddress.IPAddress.Split(".", 3) | Select-Object -Index 0,1) -join "."
 
-    $server = netsh dhcp show server | Select-String $net | ForEach-Object {$_ -split " "} | Select-Object -Index 1
-    $server = $server -replace "\[", '' -replace "\]", ''
+    if(!($Server)) {
+        $server = netsh dhcp show server | Select-String $net | ForEach-Object {$_ -split " "} | Select-Object -Index 1
+        $server = $server -replace "\[", '' -replace "\]", ''
+    }
+
     #Open simsession to dhcpserver because DHCP server cmdlets dose not support Credentail parameter
     try {
         $session = New-CimSession -ComputerName $server -Credential $Credential -ErrorAction Stop
     } catch [Microsoft.Management.Infrastructure.CimException] {
         throw "Access is denied. Use DHCP Admin credentials"
     }
+
     #Find scopes for this DHCP server
     $scopes = Get-DhcpServerv4Scope -CimSession $session
     #For each scope parse DHCP leases and find matching Name
